124dcdc4574322b549ba4f8ef9cf67a975e6f361
AnarkeyV <kuhei.raiza@gmail.com>
2026-04-08 04:13:20 +0000

    Changed Flask script database to SQLite file within site

--- flask_app.py
+++ flask_app.py
@@ -7,14 +7,7 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="sql12822549",
-    password="lWHEVBRip6",
-    hostname="sql12.freesqldatabase.com",
-    databasename="sql12822549",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/khairulirizal/mysite/comments.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
