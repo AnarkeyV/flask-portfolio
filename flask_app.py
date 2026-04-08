da280e0d723c5848a1bf9df59ffbc5ed97948901
AnarkeyV <kuhei.raiza@gmail.com>
2026-04-08 04:16:41 +0000

    Fixed spelling error for database path

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/khairulirizal/mysite/comments.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/khairulrizal/mysite/comments.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
