4240dd8e59cee05ad2678b2b9beea5592d5298a8
AnarkeyV <kuhei.raiza@gmail.com>
2026-04-08 06:21:18 +0000

    Added my profile as the main page!

--- flask_app.py
+++ flask_app.py
@@ -23,7 +23,7 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
+        return render_template("profile.html", comments=Comment.query.all())
 
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
