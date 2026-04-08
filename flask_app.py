6cdd65eee3fbb81bae499fd4e188633b77222578
AnarkeyV <kuhei.raiza@gmail.com>
2026-04-08 03:48:11 +0000

    Added a free SQL database

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,23 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="sql12822549",
+    password="lWHEVBRip6",
+    hostname="sql12.freesqldatabase.com",
+    databasename="sql12822549",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 
